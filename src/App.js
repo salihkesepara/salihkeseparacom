2c879e52eaa3534a22d2612224a6ffc6374c5aa7
Salih Kesepara <salihkesepara@gmail.com>
2022-11-02 21:24:39 +0300

    PMD-1534 test commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Dev
+          PMD-1534 test commit
         </p>
         <a
           className="App-link"
