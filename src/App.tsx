7c292ebfc96ccf791a621deb60721925fb843471
Salih Kesepara <salihkesepara@gmail.com>
2022-11-03 23:04:33 +0300

    env-cmdrc added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import logo from 'src/assets/images/logo.svg';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,7 +15,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'src/assets/styles/App.scss';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'src/assets/styles/App.scss'
+import App from './App'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>)
