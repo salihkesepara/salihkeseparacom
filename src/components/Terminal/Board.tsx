a892e7fcbb419c8cc2335d5ac3e06417d6f50372
Salih Kesepara <salihkesepara@gmail.com>
2022-11-08 12:35:00 +0300

    scrollTo added

--- src/components/Terminal/Board.tsx
+++ src/components/Terminal/Board.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react'
 import { BoardProps } from 'src/components/Terminal/dts/board'
 
 const Board = (props: BoardProps) => {
   const { lineList } = props
+
+  useEffect(() => {
+    window.scrollTo(0, document.body.scrollHeight)
+  }, [lineList])
+
   return (
     <div id="board">
       {lineList.map((item: Function, key: number) => item(key))}
